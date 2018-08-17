1690578f6fefcdaaa96257f3de55cdd609d61782
JeffVega <jeff.vega321@gmail.com>
2018-08-17 16:05:10 -0700

    update request with cors

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
 import React, { Component } from 'react';
-
+import {REACT_APP_YELP_KEY } from './config'
 import './App.css';
 
 class App extends Component {
-  constructor () {
-    super();
-    this.state = {
-      pictures: []
+  constructor(props) {
+    super(props)
+    this.state = {suggestion: []}
+
+}
+
+
+
+
+fetchData = () => {
+  console.log('this is our',REACT_APP_YELP_KEY)
+  fetch("https://fast-beach-47884.herokuapp.com/https://api.yelp.com/v3/businesses/search?categories=food&sort_by=rating&limit=10&latitude=34.048203&longitude=-118.258949&radius=805", {
+    method: "GET",
+    dataType: "JSON",
+    headers: {
+      Authorization: `Bearer ${REACT_APP_YELP_KEY}`
     }
-  }
-  componentWillMount () {
-    fetch('http://whatever/profile')
-      .then((results) => {
-        return results.json();
-      
-      this.setState({ name: 'Andrej '});
-    })
-  }
+  })
+  .then((results) => {
+    return results.json()
+  }) 
+  .then((data) => {
+    console.log(data)                
+  })
+  .catch((error) => {
+    console.log(error, "catch the hoop")
+  })
+}
+
   render() {
     return (
       <div className="App">
@@ -26,7 +41,7 @@ class App extends Component {
           {/* <h3>L U N C H</h3> */}
         </header>
         <p className="App-intro">
-          <button onClick={}>
+          <button onClick={this.fetchData}>
               WHAT'S FOR LUNCH
           </button>
         </p>
