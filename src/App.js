f31e4081bf4bb9f245195735ae55e02352e4c8db
Julie K <julieskim160@gmail.com>
2018-08-18 18:35:37 -0700

    yelp api key

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ fetchData = () => {
         <header className="App-header">
          
           <h1 className="App-title">N O R D S T R O M  R A C K | H A U T E L O O K</h1>
-          {/* <h3>L U N C H</h3> */}
+          
         </header>
         <p className="App-intro">
           <button onClick={this.fetchData}>
