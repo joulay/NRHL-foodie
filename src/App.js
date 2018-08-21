a8d9c2e836739350557692ef4ff2c59bda7f6131
Julie K <julieskim160@gmail.com>
2018-08-20 17:00:53 -0700

    edit button

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
            
-            <h1 className="App-title">N O R D S T R O M  R A C K | H A U T E L O O K</h1>
+            <h1 className="App-title">HACK NIGHT CHALLENGE</h1>
             
           </header>
           <div className="content">
